1eeea1890154b15bb8df9fa838a336a279499c3c
Kathy Hudson <khitx@icloud.com>
2018-01-26 19:58:47 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+
+  def initialize(name)
+      @name = name
+      @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[:grade] = [] unless @roster[:grade]
+    @roster[:grade] << student_name      
+  end
+  
+end #class School
