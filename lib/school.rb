48985210c4975571c5a268e70aca35dccaf27cc5
Kathy Hudson <khitx@icloud.com>
2018-01-26 20:16:48 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,10 +6,10 @@ class School
       @roster = {}
   end
 
-  #def add_student(student_name, grade)
-    #@roster[:grade] = [] unless @roster[:grade]
-    #@roster[:grade] << student_name
-  #end
+  def add_student(student_name, grade)
+    @roster[:grade] = [] unless @roster[:grade]
+    @roster[:grade] << student_name
+  end
 
   #def grade(grade)
     #@roster[:grade]
